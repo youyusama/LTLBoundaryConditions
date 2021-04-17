d2267c54d60c1cfac226dbea479fde11fa8a480d
youyu <474323375@qq.com>
2021-04-17 14:52:20 +0800

    gore class

--- solve_bc.py
+++ solve_bc.py
@@ -3,4 +3,7 @@ sys.path.append('/usr/local/lib/python3.8/site-packages')
 import spot
 from aalta import sat_check_aalta
 
-print(sat_check_aalta('a U b'))
+# print(sat_check_aalta('[] (q -> s)'))
+f = spot.formula('G (q->s)')
+print(f.to_str('spin', parenth=True))
+
